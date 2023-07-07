24bfde65e490e2831af002aec798b24da8692734
incre <incregeni@gmail.com>
2023-07-08 00:58:10 +0900

    feat: use uint64 to save genome

--- contracts/NftPfp.sol
+++ contracts/NftPfp.sol
@@ -1,33 +1,28 @@
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-
 import "hardhat/console.sol";
 
-contract NftPfp is ERC721, Ownable {
+contract NftPfp {
     // tokenId -> genome
-    uint72[] public genomes;
+    uint64[] public packedGenomes;
 
-    constructor() ERC721("NftPfp", "PFP") {}
+    constructor() {}
 
-    function mint(address to, uint8[] memory _genomes) public onlyOwner {
-        genomes.push(packGenome(_genomes));
-
-        _mint(to, genomes.length - 1);
+    function mint(uint64 _packedGenome) public {
+        packedGenomes.push(_packedGenome);
     }
 
     function packGenome(
-        uint8[] memory _genome
-    ) public pure returns (uint72 packedGenome) {
+        uint64[] memory _genome
+    ) public view returns (uint64 packedGenome) {
         require(_genome.length == 12, "Invalid genome length");
 
         packedGenome =
-            (_genome[0] << 59) |
-            (_genome[1] << 54) |
-            (_genome[2] << 50) |
-            (_genome[3] << 44) |
-            (_genome[4] << 40) |
+            (_genome[0] << 58) |
+            (_genome[1] << 53) |
+            (_genome[2] << 49) |
+            // size of (_genome[4], _genome[3]) = 10 but reduce it to 9
+            ((_genome[3] * 11 + _genome[4]) << 40) |
             (_genome[5] << 33) |
             (_genome[6] << 27) |
             (_genome[7] << 21) |
@@ -38,33 +33,35 @@ contract NftPfp is ERC721, Ownable {
     }
 
     function unpackGenome(
-        uint72 packedGenome
-    ) public view returns (uint8[] memory) {
+        uint64 packedGenome
+    ) public pure returns (uint8[] memory) {
         uint8[] memory _genome = new uint8[](12);
 
-        _genome[11] = uint8(packedGenome & 0x2F);
+        _genome[11] = uint8(packedGenome & 0x1F);
         packedGenome = packedGenome >> 5;
-        _genome[10] = uint8(packedGenome & 0x2F);
+        _genome[10] = uint8(packedGenome & 0x1F);
         packedGenome = packedGenome >> 5;
-        _genome[9] = uint8(packedGenome & 0x1F);
+        _genome[9] = uint8(packedGenome & 0x0F);
         packedGenome = packedGenome >> 4;
-        _genome[8] = uint8(packedGenome & 0x8F);
+        _genome[8] = uint8(packedGenome & 0x7F);
         packedGenome = packedGenome >> 7;
-        _genome[7] = uint8(packedGenome & 0x4F);
+        _genome[7] = uint8(packedGenome & 0x3F);
         packedGenome = packedGenome >> 6;
-        _genome[6] = uint8(packedGenome & 0x4F);
+        _genome[6] = uint8(packedGenome & 0x3F);
         packedGenome = packedGenome >> 6;
-        _genome[5] = uint8(packedGenome & 0x8F);
+        _genome[5] = uint8(packedGenome & 0x7F);
         packedGenome = packedGenome >> 7;
-        _genome[4] = uint8(packedGenome & 0x0F);
-        packedGenome = packedGenome >> 4;
-        _genome[3] = uint8(packedGenome & 0x4F);
-        packedGenome = packedGenome >> 6;
+
+        // size of (_genome[4], _genome[3]) = 10 but reduce it to 9
+        _genome[4] = uint8((packedGenome & 0x1FF) % 11);
+        _genome[3] = uint8((packedGenome & 0x1FF) / 11);
+        packedGenome = packedGenome >> 9;
+
         _genome[2] = uint8(packedGenome & 0x0F);
         packedGenome = packedGenome >> 4;
         _genome[1] = uint8(packedGenome & 0x1F);
         packedGenome = packedGenome >> 5;
-        _genome[0] = uint8(packedGenome & 0x4F);
+        _genome[0] = uint8(packedGenome & 0x3F);
 
         return _genome;
     }
--- test/NftPfp.test.ts
+++ test/NftPfp.test.ts
@@ -7,8 +7,17 @@ describe("NftPfp", function () {
   let owner: any;
   let player1: any;
   let player2: any;
-  let genomes: number[] = [];
+  let genomes = [];
+  let unpackedGenomes = [];
   let genomeRange = [60, 30, 10, 40, 10, 100, 50, 60, 100, 10, 25, 30];
+
+  const makeRandomUnpackedGenome = () => {
+    let unpackedGenome = [];
+    for (let j = 0; j <= 11; j++) {
+      unpackedGenome.push(Math.floor(Math.random() * (genomeRange[j] + 1)));
+    }
+    return unpackedGenome;
+  };
 
   before(async function () {
     [owner, player1, player2] = await hre.ethers.getSigners();
@@ -24,22 +33,33 @@ describe("NftPfp", function () {
   it("mint 50000 NFTs", async function () {
     this.timeout(600000);
     for (let i = 0; i < 5000; i++) {
-      let genome = [];
-      for (let j = 11; j >= 0; j--) {
-        genome.push(Math.floor(Math.random() * (genomeRange[j] + 1)));
-      }
-      await NftPfp.mint(player1.address, genome);
-      genomes.push(await NftPfp.packGenome(genome));
+      let unpackedGenome = makeRandomUnpackedGenome();
+      await NftPfp.mint(await NftPfp.packGenome(unpackedGenome));
+      genomes.push(await NftPfp.packGenome(unpackedGenome));
+      unpackedGenomes.push(unpackedGenome);
     }
-    expect(await NftPfp.balanceOf(player1.address)).to.equal(5000);
   });
   it("check the genomes", async function () {
+    this.timeout(600000);
     for (let i = 0; i < 5000; i++) {
-      expect(await NftPfp.genomes(i)[0]).to.equal(await NftPfp.unpackGenome(genomes[i])[0]);
-      expect(await NftPfp.genomes(i)[3]).to.equal(await NftPfp.unpackGenome(genomes[i])[3]);
-      expect(await NftPfp.genomes(i)[5]).to.equal(await NftPfp.unpackGenome(genomes[i])[5]);
-      expect(await NftPfp.genomes(i)[8]).to.equal(await NftPfp.unpackGenome(genomes[i])[8]);
-      expect(await NftPfp.genomes(i)[11]).to.equal(await NftPfp.unpackGenome(genomes[i])[11]);
+      // const packedGenomeString = (await NftPfp.unpackGenome(await NftPfp.packedGenomes(i))).map(
+      //   (it) => {
+      //     console.log(it);
+      //     it.toNumber();
+      //   }
+      // );
+      // const packedGenomeString1 = unpackedGenomes[i].map((it) => {
+      //   it.toNumber();
+      // });
+      // console.log(packedGenomeString);
+      console.log(i);
+      console.log(unpackedGenomes[i]);
+      console.log(await NftPfp.unpackGenome(await NftPfp.packedGenomes(i)));
+      console.log(genomes[i]);
+      console.log(await NftPfp.packedGenomes(i));
+      expect(await NftPfp.unpackGenome(await NftPfp.packedGenomes(i))).to.deep.eq(
+        unpackedGenomes[i]
+      );
     }
   });
 });
